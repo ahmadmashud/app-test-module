7fdce6a2ee0f080122b32d3972dce019619aba24
ahmad <ahmad@impacto.id>
2022-10-02 11:19:32 +0700

    app using own module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ahmadmashud/app-test-module
+
+go 1.18
+
+require github.com/ahmadmashud/go-module-test v1.0.3 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	TestOke "github.com/ahmadmashud/go-module-test"
+)
+
+func main() {
+	fmt.Println(TestOke.TestOke())
+}
